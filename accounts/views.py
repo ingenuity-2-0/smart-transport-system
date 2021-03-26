a572a70796f0224683964383fd4bc0289986d7b4
Sabbir Hosen <18201076@uap-bd.edu>
2021-03-26 22:04:35 +0600

    Made the navigation bar dynamic

--- accounts/views.py
+++ accounts/views.py
@@ -4,28 +4,30 @@ from django.contrib import messages
 import re
 from django.http import HttpResponseRedirect
 
+
 # Create your views here.
 
+
 def account(request):
-    return render(request, 'accounts/loginform.html')  # renderlogin form
+    return render(request, 'accounts/loginform.html')  # render login form
 
 
 def login(request):
     user_name = request.POST['loginemail']
     password = request.POST['loginPassword']
-    #------------------------------
+    # ------------------------------
     values = {
         'user_name': user_name,
     }
-    #------------------------------
-    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'     # mail format
-    if re.search(regex, user_name): # imported re | checking the mail format
-        user = auth.authenticate(email=user_name, password=password)    #taking mail as user_name
+    # ------------------------------
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'  # mail format
+    if re.search(regex, user_name):  # imported re | checking the mail format
+        user = auth.authenticate(email=user_name, password=password)  # taking mail as user_name
     else:
         user = auth.authenticate(username=user_name, password=password)
-    if user is not None:    #user created
-        auth.login(request, user)   # loged in 
-        return redirect('/')    # to home
+    if user is not None:  # user created
+        auth.login(request, user)  # loged in
+        return redirect('/')  # to home
     else:
         message = {
             'from': 'login',
@@ -36,33 +38,34 @@ def login(request):
     # return render(request, './accounts/login.html', {'data': 'login is successful'})
 
 
-def register(request):  #getting new user info
+def register(request):  # getting new user info
     user_name = request.POST['username']
     full_name = str(request.POST['full_name'])
     email = request.POST['emailAddress']
     password = request.POST['password']
-    #-----------------------------------
+    # -----------------------------------
     values = {
         'user_name': user_name,
         'full_name': full_name,
         'email': email,
     }
-    #-----------------------------------
-    first_name, last_name = full_name.rsplit(maxsplit=1)    # splitting name as reverse order 
-    if User.objects.filter(username=user_name).exists():    # checking user name from database  
+    # -----------------------------------
+    first_name, last_name = full_name.rsplit(maxsplit=1)  # splitting name as reverse order
+    if User.objects.filter(username=user_name).exists():  # checking user name from database
         message = {
             'from': 'signup',
             'error': 'User name is already taken',
-            'values': values    # to stay in form
+            'values': values  # to stay in form
         }
-        return render(request, 'accounts/loginform.html', message)  #return with message
+        return render(request, 'accounts/loginform.html', message)  # return with message
     else:
-        user = User.objects.create_user(username=user_name, password=password, email=email, first_name=first_name, last_name=last_name)
+        user = User.objects.create_user(username=user_name, password=password, email=email, first_name=first_name,
+                                        last_name=last_name)
         user.save()
-        return redirect('/')    # to home
+        return redirect('/')  # to home
     # return render(request, 'accounts/login.html', {'data': 'registration is successful'})
 
 
 def logout(request):
     auth.logout(request)
-    return redirect('/')    # to home
+    return redirect('/')  # to home
