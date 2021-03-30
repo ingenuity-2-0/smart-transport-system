cfeac29a63e16376af3f4a29d8aba2f5c2433daa
Sabbir Hosen <18201076@uap-bd.edu>
2021-03-30 09:13:31 +0600

    Insert data of all the database in this project requried

--- home_page/models.py
+++ home_page/models.py
@@ -1,21 +1,23 @@
 from django.db import models
 
-# 3 models are created 
+
+# 3 models are created
 
 class Route(models.Model):
-    route_id = models.AutoField(primary_key=True)  #auto generated
-    routes = models.TextField() #place name as text
+    route_id = models.AutoField(primary_key=True)  # auto generated
+    routes = models.TextField()  # place name as text
 
 
 class BusInformation(models.Model):
-    bus_id = models.AutoField(primary_key=True) #auto generated
-    bus_name = models.CharField(max_length=255)   #bus name 
-    bus_type = models.CharField(max_length=15) # bus type
-    route_id = models.ForeignKey('Route', on_delete=models.CASCADE) #added foreignKey from Route Table
+    bus_id = models.AutoField(primary_key=True)  # auto generated
+    bus_name = models.CharField(max_length=255)  # bus name
+    bus_sourcetodestination = models.CharField(max_length=80)  # Source to destination
+    bus_viaroad = models.TextField()  # for regular expression operation
+    bus_type = models.CharField(max_length=15)  # bus type
+    route_id = models.ForeignKey('Route', on_delete=models.CASCADE)  # added foreignKey from Route Table
 
 
 class Map(models.Model):
-    map_id = models.AutoField(primary_key=True) #map id 
-    bus_id = models.ForeignKey('BusInformation', on_delete=models.CASCADE)  #added foreignKey from BusInformation Table
-    way_points = models.TextField() #waypoint as text
-
+    map_id = models.AutoField(primary_key=True)  # map id
+    bus_id = models.ForeignKey('BusInformation', on_delete=models.CASCADE)  # added foreignKey from BusInformation Table
+    way_points = models.TextField()  # waypoint as text
