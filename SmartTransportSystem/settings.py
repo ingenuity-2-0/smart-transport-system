293a2cd02f0a718097b64d7d96561f7855a3c031
Sabbir Hosen <18201076@uap-bd.edu>
2021-04-15 00:56:12 +0600

    Reset password Feature added

--- SmartTransportSystem/settings.py
+++ SmartTransportSystem/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     # custom installed apps
     'smartTracking',
     'accounts',
@@ -122,3 +123,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')    # to dynamic all static files 
 ]
+LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('Email_user')
+EMAIL_HOST_PASSWORD = os.environ.get('Email_password')
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,33 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path('', views.account, name='accounts'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='accounts/submit.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='accounts/checkmail.html'),
+         name='password_reset_done'),
+    path('password-reset-cofirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='accounts/confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='accounts/emptyPage.html'),
+         name='password_reset_complete'),
+    # path('password-reset/',
+    #      auth_views.PasswordResetView.as_view(),
+    #      name='password_reset'),
+    # path('password-reset/done/',
+    #      auth_views.PasswordResetDoneView.as_view(),
+    #      name='password_reset_done'),
+    # path('password-reset-cofirm/<uidb64>/<token>/',
+    #      auth_views.PasswordResetConfirmView.as_view(),
+    #      name='password_reset_confirm'),
+    # path('password-reset-complete/',
+    #      auth_views.PasswordResetCompleteView.as_view(),
+    #      name='password_reset_complete'),
 ]
